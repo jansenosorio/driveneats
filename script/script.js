6de59ee5078258c60bc8838e85e3c5fc8f7e60a0
jansenosorio <dev.jansen93@gmail.com>
2022-10-12 20:45:04 -0300

    versao final com bordas js concluida

--- script/script.js
+++ script/script.js
@@ -1,9 +1,27 @@
-function selecionarPrato(item) {
-  const pratoPrincipal = document.querySelector('.borda')
+function selecionarPrato(selecao) {
+  const pratoPrincipal = document.querySelector('.borda-prato')
 
   if (pratoPrincipal !== null) {
-    pratoPrincipal.classList.remove('borda')
+    pratoPrincipal.classList.remove('borda-prato')
   }
 
-  item.classList.toggle('borda')
+  selecao.classList.toggle('borda-prato')
+}
+
+function selecionarBebida(selecao) {
+  const bebidaPrincipal = document.querySelector('.borda-bebidas')
+  if (bebidaPrincipal !== null) {
+    bebidaPrincipal.classList.remove('borda-bebidas')
+  }
+
+  selecao.classList.toggle('borda-bebidas')
+}
+
+function selecionarSobremesa(selecao) {
+  const sobremesa = document.querySelector('.borda-sobremesa')
+  if (sobremesa !== null) {
+    sobremesa.classList.remove('borda-sobremesa')
+  }
+
+  selecao.classList.toggle('borda-sobremesa')
 }
